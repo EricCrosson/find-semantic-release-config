a3cd8fdc3a33a94c7799902af913f6a396bd4a81
Eric Crosson <eric.s.crosson@utexas.edu>
2023-05-13 17:43:04 -0500

    fix: derive Debug
    instead of using a custom representation. I think this is more suitable
    becaues it lets binary crates determine the appropriate format for
    displaying errors (perhaps using
    https://github.com/EricCrosson/little-anyhow).

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
+#[derive(Debug)]
 pub enum Error {
     /// Unable to open file
     FileOpenError { source: io::Error, path: PathBuf },
@@ -40,25 +41,6 @@ impl std::fmt::Display for Error {
                 write!(f, "unable to parse file {:?}", path)
             }
         }
-    }
-}
-
-fn error_chain_fmt(
-    e: &impl std::error::Error,
-    f: &mut std::fmt::Formatter<'_>,
-) -> std::fmt::Result {
-    writeln!(f, "{}\n", e)?;
-    let mut current = e.source();
-    while let Some(cause) = current {
-        writeln!(f, "Caused by:\n\t{}", cause)?;
-        current = cause.source();
-    }
-    Ok(())
-}
-
-impl std::fmt::Debug for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
     }
 }
 
